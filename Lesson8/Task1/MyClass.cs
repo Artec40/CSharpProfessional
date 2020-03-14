de30b504ad9669acf07aa8d246026b9080739d31
dnwdark <Artec40@gmail.com>
2020-03-14 17:00:08 +0500

    Lesson8

---
+++ Lesson8/Task1/MyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+    [XmlRoot("className")]
+    public class MyClass
+    {
+        private string propertyOne = "firstProperty";
+        private string propertyTwo = "secondProperty";
+
+        [XmlElement("firstProperty")]
+        public string PropertyOne
+        {
+            get { return propertyOne; }
+            set { propertyOne = value; }
+        }
+
+        [XmlElement("secondProperty")]
+        public string PropertyTwo
+        {
+            get { return propertyTwo;}
+            set { propertyTwo = value;}
+
+        }
+
+        public MyClass()
+        {
+            
+        }
+        
+        public MyClass(string propOne, string propTwo)
+        {
+            this.PropertyOne = propOne;
+            this.PropertyTwo = propTwo;
+        }
+        
+        
+    }
+}
---
+++ Lesson8/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(MyClass));
+
+            MyClass myClass = new MyClass();
+            
+            FileStream stream = new FileStream
+            ("Serialization.XML", 
+                FileMode.Create, FileAccess.Write, FileShare.Read);
+            
+            serializer.Serialize(stream,myClass);
+            stream.Close();
+        }    
+    }
+}
