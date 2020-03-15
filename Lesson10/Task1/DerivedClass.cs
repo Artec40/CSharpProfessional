99ec4f2a3a3d13d60e96e8b9b6f234b0163e991c
dnwdark <Artec40@gmail.com>
2020-03-15 17:22:33 +0500

    Lesson10

---
+++ Lesson10/Task1/DerivedClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    public class DerivedClass : BaseClass
+    {
+        public new void NonVirtualInterfaceMethod()
+        {
+            FirstMethod();
+            SecondMethod();
+        }
+        // Замещение метода. Инкапсуляция не работает.
+        protected new void FirstMethod()
+        {
+            Console.WriteLine("First method in derived class");
+        }
+
+        // Переопределение метода.
+        protected override void SecondMethod()
+        {
+            Console.WriteLine("Second method in derived class");
+        }
+    }
+}
---
+++ Lesson10/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass myClass1 = new DerivedClass();
+            
+            // Шаблон NVI, позволяющий создать версионность.
+            myClass1.NonVirtualInterfaceMethod();
+
+            DerivedClass myClass2 = new DerivedClass();
+            myClass2.NonVirtualInterfaceMethod();
+        }
+    }
+}
