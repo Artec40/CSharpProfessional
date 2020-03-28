78b99113abb20ed9f4d2928b2fd71be20d30768d
dnwdark <Artec40@gmail.com>
2020-03-28 21:25:31 +0500

    Lesson14

---
+++ Lesson14/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void Task1()
+        {
+            Console.WriteLine($"Task 1 thread - {Thread.CurrentThread.ManagedThreadId.ToString()}");
+            for (int i = 0; i < 40; i++)
+            {
+                Thread.Sleep(50);
+                Console.Write("+");
+            }
+        }
+        
+        public static void Task2()
+        {
+            Console.WriteLine($"Task 2 thread - {Thread.CurrentThread.ManagedThreadId.ToString()}");
+            for (int i = 0; i < 40; i++)
+            {
+                Thread.Sleep(50);
+                Console.Write("-");
+            }
+        }
+        
+        static void Main(string[] args) 
+        {
+            Console.WriteLine($"Main thread - {Thread.CurrentThread.ManagedThreadId.ToString()}");
+            Parallel.Invoke(Task1,Task2);
+        }
+    }
+}
